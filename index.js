c6c45ea05bce00d088d5d9f520e07fb6d8bdc99b
juaniB12 <bjuan.ignacio11@gmail.com>
2024-04-24 16:37:49 -0300

    add loader

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const media = Array.from(document.querySelectorAll('img, video'));
+  
+    Promise.all(media.map(function(m) {
+      return new Promise(function(res, rej) {
+        if(m.tagName == 'IMG') {
+          m.addEventListener('load', res);
+        }
+        if(m.tagName == 'VIDEO') {
+          m.addEventListener('loadeddata', res);
+        }
+        setTimeout(function() {
+          res();
+        }, 3000)
+      })
+    }))
+    .then(function() {
+      document
+        .querySelectorAll('#preloader')
+        .forEach(function(preloader) {
+          preloader.classList.add('hidden');
+          document.body.classList.remove('is-loading');
+        });
+    });
+  });
+
+//-------------------------------------
+
 const form = document.querySelector('[data-form]'),
       mails = form.querySelectorAll('input[type="radio"]');
 
